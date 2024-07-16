8cc8b97bc7a3f1931b07c671f5e74ecfe11bb78d
dylanR96 <dylan.ravenson@gmail.com>
2024-07-16 19:23:18 +0200

    Installed concurrently and corrected main and app to run properly

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello there!</h1>
+      <h1>Hello, Vite + React + TypeScript!</h1>
+      <h2>Hello</h2>
     </div>
   );
 };
+
+export default App;
