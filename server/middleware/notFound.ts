60cf1e122e45de9ad4bdc3c49c7d8a68e2e415a7
dylanR96 <dylan.ravenson@gmail.com>
2024-08-01 17:47:36 +0200

    added new packages and fresh start, added new server file

--- server/middleware/notFound.ts
+++
@@ -1,9 +0,0 @@
-// Middleware for 404 errors
-
-const notFound = (req, res, next) => {
-  const error = new Error("URL not found");
-  error.status = 404;
-  next(error);
-};
-
-export default notFound;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import "./App.css";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div>
-      <h1>Hello, Vite + React + TypeScript!</h1>
-      <h2>Hello</h2>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
